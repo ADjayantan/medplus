23e47c4317f62b3aa8d15e145f75d0f34c9f0e2e
Jayantan AD <adjayantan2007@gmail.com>
2026-04-19 08:40:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ const ALLOWED_ORIGINS = [
   'http://127.0.0.1:3000',
   'http://localhost:5500',
   'http://127.0.0.1:5500',
+  'https://adjayantan.github.io',   // GitHub Pages deployment
   process.env.FRONTEND_URL || '',
 ].filter(Boolean);
 
